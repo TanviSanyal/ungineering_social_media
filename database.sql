1a79f3f9c8cd1d68f4bcddb8ae7ef97b1cb32add
tanvi <tanvisanyal111@gmail.com>
2019-07-11 20:50:46 +0530

    database

---
+++ database.sql
@@ -0,0 +1,42 @@
+CREATE TABLE users(
+    id INT NOT NULL AUTO_INCREMENT,
+    name VARCHAR(255) NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL,
+    college_name VARCHAR(255),
+    phone_number INT,
+    PRIMARY KEY(id)
+ );
+ 
+ 
+
+ 
+ 
+ 
+ 
+CREATE TABLE posts(
+    id INT NOT NULL AUTO_INCREMENT,
+    user_id INT,
+    status VARCHAR(255) NOT NULL,
+    date DATE NOT NULL,
+    time DATETIME DEFAULT CURRENT_TIMESTAMP,
+    PRIMARY KEY(id),
+    FOREIGN KEY(user_id) REFERENCES users(id)
+ );
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+
+ 
+ 
+ 
+ 
+
+ 
+ 
+ 
